8a06d0fbcc48247ac1c2e1bd92813165bd7868ee
terencewytat <terence.wy.tat@gmail.com>
2021-07-20 13:47:13 -0600

    added scroll rect fix for input fields

---
+++ Assets/Scripts/FixScrollRect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FixScrollRect: MonoBehaviour, IBeginDragHandler,  IDragHandler, IEndDragHandler, IScrollHandler
+{
+	public ScrollRect MainScroll;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        MainScroll.OnBeginDrag(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        MainScroll.OnDrag(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        MainScroll.OnEndDrag(eventData);
+    }
+
+    public void OnScroll(PointerEventData data)
+    {
+        MainScroll.OnScroll(data);
+    }
+}
