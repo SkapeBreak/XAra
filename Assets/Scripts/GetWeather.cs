00b56e13b03068606c8f63ad86385a04270dc161
terencewytat <terence.wy.tat@gmail.com>
2021-07-22 14:09:44 -0600

    changed xml data and photo icons

--- Assets/Scripts/GetWeather.cs
+++ Assets/Scripts/GetWeather.cs
@@ -10,7 +10,7 @@ public class GetWeather : MonoBehaviour
 {   
    InputField outputArea;
 
-    void Update()
+    void Start()
     {
         StartCoroutine(GetWeatherData());
     }
@@ -20,14 +20,17 @@ public class GetWeather : MonoBehaviour
         string uri = "https://api.openweathermap.org/data/2.5/weather?lat=51.056442&lon=-114.069333&units=metric&APPID=c24bbbe6090072b0f620ea57d0ce8764";
         outputArea = GameObject.Find("Weather").GetComponent<InputField>();
 
+        
         using(UnityWebRequest webRequest = UnityWebRequest.Get(uri))
         {
+            
             yield return webRequest.SendWebRequest();
             if (webRequest.isNetworkError|| webRequest.isHttpError)
             {
                 Debug.Log(webRequest.error);
             } else
             {
+                System.Threading.Thread.Sleep(5000);
                 var weather = JsonConvert.DeserializeObject<weatherResponse>(webRequest.downloadHandler.text);
                 outputArea.text = weather.main.temp.ToString() + "°C";
             }
