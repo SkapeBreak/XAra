dcc4d4c39c0b28f7db565da488ded4f0a08d4156
AndyH <andyb6@gmail.com>
2021-08-05 15:16:48 -0600

    changed ui

--- Assets/Scripts/RestaurantButton.cs
+++ Assets/Scripts/RestaurantButton.cs
@@ -6,10 +6,10 @@ using UnityEngine.UI;
 public class RestaurantButton : MonoBehaviour
 {
     [SerializeField] private Text restaurantName;
-    [SerializeField] private Text restaurantAddress;
-    [SerializeField] private Text restaurantPhone;
+    // [SerializeField] private Text restaurantAddress;
+    // [SerializeField] private Text restaurantPhone;
     [SerializeField] private Text restaurantSchedule;
-    [SerializeField] private Text restaurantMenu;
+    // [SerializeField] private Text restaurantMenu;
     [SerializeField] private Text restaurantRating;
 
     public void SetRestaurantName(string textString) 
@@ -17,25 +17,25 @@ public class RestaurantButton : MonoBehaviour
         restaurantName.text = textString;
     }
 
-    public void SetRestaurantAddress(string textString) 
-    {
-        restaurantAddress.text = textString;
-    }
+    // public void SetRestaurantAddress(string textString) 
+    // {
+    //     restaurantAddress.text = textString;
+    // }
 
-    public void SetRestaurantPhone(string textString) 
-    {
-        restaurantPhone.text = textString;
-    }
+    // public void SetRestaurantPhone(string textString) 
+    // {
+    //     restaurantPhone.text = textString;
+    // }
 
     public void SetRestaurantSchedule(string textString) 
     {
         restaurantSchedule.text = textString;
     }
 
-    public void SetRestaurantMenu(string textString) 
-    {
-        restaurantMenu.text = textString;
-    }
+    // public void SetRestaurantMenu(string textString) 
+    // {
+    //     restaurantMenu.text = textString;
+    // }
 
     public void SetRestaurantRating(string textString) 
     {
