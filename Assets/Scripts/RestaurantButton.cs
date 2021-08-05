f833ef3ea006389eb46e91da24290db72991b707
AndyH <andyb6@gmail.com>
2021-08-05 15:17:35 -0600

    changed ui

--- Assets/Scripts/RestaurantButton.cs
+++ Assets/Scripts/RestaurantButton.cs
@@ -6,10 +6,7 @@ using UnityEngine.UI;
 public class RestaurantButton : MonoBehaviour
 {
     [SerializeField] private Text restaurantName;
-    // [SerializeField] private Text restaurantAddress;
-    // [SerializeField] private Text restaurantPhone;
     [SerializeField] private Text restaurantSchedule;
-    // [SerializeField] private Text restaurantMenu;
     [SerializeField] private Text restaurantRating;
 
     public void SetRestaurantName(string textString) 
@@ -17,25 +14,10 @@ public class RestaurantButton : MonoBehaviour
         restaurantName.text = textString;
     }
 
-    // public void SetRestaurantAddress(string textString) 
-    // {
-    //     restaurantAddress.text = textString;
-    // }
-
-    // public void SetRestaurantPhone(string textString) 
-    // {
-    //     restaurantPhone.text = textString;
-    // }
-
     public void SetRestaurantSchedule(string textString) 
     {
         restaurantSchedule.text = textString;
     }
-
-    // public void SetRestaurantMenu(string textString) 
-    // {
-    //     restaurantMenu.text = textString;
-    // }
 
     public void SetRestaurantRating(string textString) 
     {
